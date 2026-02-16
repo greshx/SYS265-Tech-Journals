c621aa514a41e085e27d2fc9e123b654481acbbd
greshx <greshx@millermanly77@gmail.com>
2026-02-16 06:37:22 +0000

    modified ssh script

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -1,6 +1,22 @@
+#!/bin/bash
+
 # secure-ssh.sh
 # author greshx
 # creates new ssh user using $1 parameter
 # adds a public key from the local repo or curled from the remote repo
 # removes roots ability to ssh in
-echo "ALL YOUR CODE GOES HERE"
+
+# creates $1 user and adds public key to them
+sudo useradd -m -d /home/${1} -s /bin/bash ${1}
+sudo mkdir /home/${1}/.ssh
+cd /home/miller/SYS265-Tech-Journals
+sudo cp /home/miller/SYS265-Tech-Journals/linux/public-keys/id_rsa.pub /home/${1}/.ssh/authorized_keys
+sudo chmod 700 /home/${1}/.ssh
+sudo chmod 600 /home/${1}/.ssh/authorized_keys
+sudo chown -R ${1}:${1} /home/${1}/.ssh
+
+# remove root ssh login
+sudo sed -i 's/^PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
+
+# restart ssh
+sudo systemctl restart ssh
